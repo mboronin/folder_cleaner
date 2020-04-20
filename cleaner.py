b8a4033c648ebfa7762067a47e54c8ab4dfcff30
Mikhail Boronin <boroninm@gmail.com>
2020-04-21 00:07:05 +0200

    feat: added folder search

--- cleaner.py
+++ cleaner.py
@@ -1,18 +1,54 @@
 import os
 import subprocess
+import sys
 
 '''
 gets an absolute path to a folder
 '''
-def main(arg):
-    cleaner = MyCleaner(arg)
+
+
+def main(path):
+    try:
+        folder = find_folder(path)
+    except FileNotFoundError:
+        print("Could not locate your folder\nTerminating!")
+        return
+    cleaner = MyCleaner(folder)
     cleaner.clean()
+    print("Cleaning is now complete")
 
 
 def check_if_folder_exists(folder: str):
     if os.path.exists(folder) and os.path.isdir(folder):
         return True
     return False
+
+
+def find_folder(path: str):
+    if os.path.isabs(path):
+        if check_if_folder_exists(path):
+            return path
+        else:
+            raise FileNotFoundError
+    elif check_if_folder_exists(os.path.abspath(path)):
+        path = os.path.abspath(path)
+        print("Did you mean " + path + " [y/n]?")
+        answer = input()
+        if answer == "y":
+            return path
+        else:
+            raise FileNotFoundError
+    else:
+        homedir = os.path.expanduser("~")
+        path = homedir + '/' + path
+        if(check_if_folder_exists(path)):
+            print("Did you mean " + path + " [y/n]?")
+            answer = input()
+            if answer == "y":
+                return path
+            else:
+                raise FileNotFoundError
+
 
 
 def move_file_to_folder(filename: str, folder: str):
@@ -45,7 +81,7 @@ class MyCleaner:
                     continue
                 name, file_extension = os.path.splitext(filename)
                 extension = file_extension[1:].lower()
-                if extension_counter[extension]< 5:
+                if extension_counter[extension] < 5:
                     continue
                 ext_folder = self.folder + '/' + extension
                 if check_if_folder_exists(ext_folder):
@@ -54,4 +90,8 @@ class MyCleaner:
                     create_folder(self.folder, extension)
                     move_file_to_folder(filepath, ext_folder)
 
-main("/home/mboronin/Downloads/Telegram Desktop")
+
+if len(sys.argv) < 2:
+    print("Path not provided")
+else:
+    main(sys.argv[1])
